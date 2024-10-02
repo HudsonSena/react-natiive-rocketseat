ca262eecdc18d08170abc811b1afd81c086e92f4
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-02 11:40:44 -0300

    update project class

---
+++ sena-app001/src/screens/Home/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 5,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#ffffff",
+    padding: 16,
+    fontSize: 16,
+  },
+});
