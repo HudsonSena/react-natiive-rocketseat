f40c0a56db5683a60927b9a1fcbdf4adc38bb18a
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-02 12:21:48 -0300

    update styles

--- sena-app001/src/screens/Home/index.tsx
+++ sena-app001/src/screens/Home/index.tsx
@@ -1,18 +1,24 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {}
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
 
       <Text style={styles.eventDate}>Quarta, 02 de Outubro de 2024</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor={"#6b6b6b"}
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor={"#6b6b6b"}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- sena-app001/src/screens/Home/styles.ts
+++ sena-app001/src/screens/Home/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 5,
     backgroundColor: "#131016",
     padding: 24,
   },
@@ -18,11 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: "#1f1e25",
     borderRadius: 5,
     color: "#ffffff",
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
+  },
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31cf67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
   },
 });
