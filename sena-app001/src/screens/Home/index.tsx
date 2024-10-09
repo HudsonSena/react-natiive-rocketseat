c210c338e2ffe1f5c4c74b277ef167b6edf10763
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-08 22:03:43 -0300

    update functions

--- sena-app001/src/screens/Home/index.tsx
+++ sena-app001/src/screens/Home/index.tsx
@@ -1,8 +1,45 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
-  function handleParticipantAdd() {}
+  const participants = [
+    "Hudson",
+    "Diego",
+    "Sena",
+    "Souza",
+    "Cristina",
+    "Maria",
+    "Joao",
+    "Socorro",
+    "Harrison",
+    "Corró",
+  ];
+
+  function handleParticipantAdd() {
+    if (participants.includes("Sena")) {
+      return Alert.alert("Participante ja existe");
+    }
+  }
+  function handleParticipantRemove(name: string) {
+    Alert.alert("Remover", `Deseja remover ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () => Alert.alert("Deletado"),
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
@@ -19,6 +56,20 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+      <FlatList
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptytext}>Ninguem esta no evento</Text>
+        )}
+      />
     </View>
   );
 }
