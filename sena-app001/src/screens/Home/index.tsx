9f967358b5d3610dc60bfba95300bcf60de95bfc
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2024-10-28 09:52:15 -0300

    Update index.tsx

--- sena-app001/src/screens/Home/index.tsx
+++ sena-app001/src/screens/Home/index.tsx
@@ -8,31 +8,29 @@ import {
 } from "react-native";
 import { styles } from "./styles";
 import { Participant } from "../../components/Participant";
+import { useState } from "react";
 
 export function Home() {
-  const participants = [
-    "Hudson",
-    "Diego",
-    "Sena",
-    "Souza",
-    "Cristina",
-    "Maria",
-    "Joao",
-    "Socorro",
-    "Harrison",
-    "Corró",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantsName, setParticipantsName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("Sena")) {
+    if (participants.includes(participantsName)) {
       return Alert.alert("Participante ja existe");
     }
+
+    setParticipants((prevState) => [...prevState, participantsName]);
+    setParticipantsName("");
   }
+
   function handleParticipantRemove(name: string) {
     Alert.alert("Remover", `Deseja remover ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado"),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
@@ -51,6 +49,8 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor={"#6b6b6b"}
+          onChangeText={setParticipantsName}
+          value={participantsName}
         />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
