da18cf1e6fa77be3103003b074643bfa93630467
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-03-11 13:23:51 -0300

    update theme

--- ignite-teams-sena/src/screens/Groups/index.tsx
+++ ignite-teams-sena/src/screens/Groups/index.tsx
@@ -1,19 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styles";
+//or import * as S from './styles'
+//S.Container and S.Title
 
 export default function Groups() {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- ignite-teams-sena/src/screens/Groups/styles.ts
+++ ignite-teams-sena/src/screens/Groups/styles.ts
@@ -1,3 +1,13 @@
 import styled from "styled-components/native";
 
-export const Container = styled.Text``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_200};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: black;
+  font-size: 32;
+`;
